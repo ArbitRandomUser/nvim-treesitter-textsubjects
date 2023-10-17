11eaf048ed89ea574ec6c5356835e07a20a83ec2
John George Francis <arbitrandomuser@gmail.com>
2023-10-17 07:45:49 +0530

    julia fix

--- queries/julia/textsubjects-smart.scm
+++ queries/julia/textsubjects-smart.scm
@@ -1,10 +1,10 @@
-((comment) @_start @_end
+((line_comment) @_start @_end
      (#make-range! "range" @_start @_end))
 
 ; TODO This query doesn't work for comment groups at the start and end of a
 ; file
 ; See https://github.com/tree-sitter/tree-sitter/issues/1138
-(((_) @head . (comment) @_start . (comment)+ @_end (_) @tail)
+(((_) @head . (line_comment) @_start . (line_comment)+ @_end (_) @tail)
     (#not-has-type? @tail "comment")
     (#not-has-type? @head "comment")
     (#make-range! "range" @_start @_end))
@@ -17,7 +17,8 @@
     (if_statement)
     (try_statement)
     (do_clause)
-    (array_expression)
+    (vector_expression)
+    (tuple_expression)
 ] @_start @_end)
 (#make-range! "range" @_start @_end))
 
@@ -30,11 +31,11 @@
 ((tuple_expression (_) @_start @_end . ","? @_end)
     (#make-range! "range" @_start @_end))
 
-((array_expression (_) @_start @_end . ","? @_end)
+((vector_expression (_) @_start @_end . ","? @_end)
     (#make-range! "range" @_start @_end))
 
-((type_argument_list (_) @_start @_end . ","? @_end)
-    (#make-range! "range" @_start @_end))
+;((type_argument_list (_) @_start @_end . ","? @_end)
+;    (#make-range! "range" @_start @_end))
 
 ((return_statement (_) @_start @_end)
     (#make-range! "range" @_start @_end))
